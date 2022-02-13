817b557f0a8f04708b93c4b2de08fa8bfa217ce5
Isacc <iavitia@outlook.com>
2022-02-12 21:29:30 -0700

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+import { useDropzone } from 'react-dropzone';
+import { useCallback, useState, useEffect } from 'react';
+
+function App() {
+  const [images, setImages] = useState([]);
+
+  function handleUpload() {
+    console.log('Uploading files...');
+  }
+
+  const onDrop = useCallback((acceptedFiles, rejectedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImages((prevState) => [...prevState, reader.result]);
+      };
+      reader.readAsDataURL(file);
+    });
+    console.log('acceptedFiles', acceptedFiles);
+    console.log('rejectedFiles', rejectedFiles);
+  }, []);
+
+  useEffect(() => {
+    console.log(images);
+  }, [images]);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: 'image/png',
+  });
+
+  return (
+    <div className='App'>
+      <div className='dropzone' {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? 'Drag Active' : 'You can drop your files here.'}
+      </div>
+      {images.length > 0 && (
+        <div>
+          {images.map((image, index) => (
+            <img
+              className='selected-images'
+              src={image}
+              key={index}
+              alt={`img ${index}`}
+            />
+          ))}
+        </div>
+      )}
+      {images.length > 0 && (
+        <button onClick={handleUpload}>Upload images</button>
+      )}
+    </div>
+  );
+}
+
+export default App;
